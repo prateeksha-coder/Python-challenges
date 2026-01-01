abba4aee27a12f03cc85d11f262dd06bc951d0a2
prateeksha-coder <prateeksha.codingal@gmail.com>
2026-01-01 15:14:48 +0530

    commit

--- a2.py
+++ a2.py
@@ -4,16 +4,9 @@ correct_num=56
 total_num=40
 sum=mean1*total_num# mean=sum/total_num
 print("the sum of 40 numbers ",sum)
-num2=sum-(wrong_num-correct_num)
-print(num2)
+sum2=sum-(wrong_num-correct_num)
+print(sum2)
 
-mean2=num2/total_num
+mean2=sum2/total_num
 print(mean2)
 
-'''1,2,7,4,5
-sum=1+2+3+4+5=15
-mean/avg=15/5=3
-wrong=3
-correct=7
-sum=15-(3-7)=15-(-4)=19
-avg=19/5=3.8'''
